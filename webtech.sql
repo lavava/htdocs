776dbc3105fee3cc62516c94047c25d936d55e35
goelakash <goelakash93@gmail.com>
2015-04-19 20:29:24 +0530

    updated db

--- webtech.sql
+++ webtech.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 14, 2015 at 02:08 PM
+-- Generation Time: Apr 19, 2015 at 04:57 PM
 -- Server version: 5.5.32
 -- PHP Version: 5.4.19
 
@@ -30,6 +30,7 @@ USE `webtech`;
 
 CREATE TABLE IF NOT EXISTS `images` (
   `title` varchar(50) NOT NULL,
+  `user` varchar(40) NOT NULL,
   `artist` varchar(50) NOT NULL,
   `sold` tinyint(1) NOT NULL,
   `path` varchar(50) NOT NULL,
@@ -42,9 +43,9 @@ CREATE TABLE IF NOT EXISTS `images` (
 -- Dumping data for table `images`
 --
 
-INSERT INTO `images` (`title`, `artist`, `sold`, `path`, `time`, `id`) VALUES
-('Mona Lisa', 'Leonardo Da Vinci', 0, 'C:/xampp/htdocs/images/admin/pic02.jpg', '0000-00-00 00:00:00', 'Leopic1429'),
-('Starry Night', 'Picasso', 0, 'C:/xampp/htdocs/images/admin/pic01.jpg', '0000-00-00 00:00:00', 'Picpic');
+INSERT INTO `images` (`title`, `user`, `artist`, `sold`, `path`, `time`, `id`) VALUES
+('Mona Lisa', 'user@user.com', 'Leonardo Da Vinci', 0, 'images/user@user.com/mona.jpg', '0000-00-00 00:00:00', 'Leomon1429'),
+('The Starry Night', 'user@user.com', 'Vincent Van Gogh', 0, 'images/user@user.com/star.jpg', '0000-00-00 00:00:00', 'Vinsta1429');
 
 -- --------------------------------------------------------
 
@@ -54,17 +55,19 @@ INSERT INTO `images` (`title`, `artist`, `sold`, `path`, `time`, `id`) VALUES
 
 CREATE TABLE IF NOT EXISTS `users` (
   `username` varchar(50) NOT NULL,
-  `password` text NOT NULL,
-  `actual_password` varchar(30) DEFAULT NULL
+  `password` varchar(50) NOT NULL,
+  `first_name` varchar(50) NOT NULL,
+  `last_name` varchar(50) NOT NULL,
+  `dob` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`username`, `password`, `actual_password`) VALUES
-('user', '5f4dcc3b5aa765d61d8327deb882cf99', 'password'),
-('admin', '21232f297a57a5a743894a0e4a801fc3', 'admin');
+INSERT INTO `users` (`username`, `password`, `first_name`, `last_name`, `dob`) VALUES
+('admin@admin.com', 'admin', 'Akash', 'Goel', '1993-09-18'),
+('user@user.com', 'user', 'Anmol', 'Jaggi', '0000-00-00');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
